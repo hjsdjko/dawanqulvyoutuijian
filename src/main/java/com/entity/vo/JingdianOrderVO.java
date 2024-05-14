102435a8eb1f54671a2227f4f9c5cce2b4fa455e
jsxs <2261203961@qq.com>
2024-05-14 11:09:10 +0800

    first commit

---
+++ src/main/java/com/entity/vo/JingdianOrderVO.java
@@ -0,0 +1,165 @@
+package com.entity.vo;
+
+import com.entity.JingdianOrderEntity;
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+
+/**
+ * 旅游景点预定
+ * 手机端接口返回实体辅助类
+ * （主要作用去除一些不必要的字段）
+ */
+@TableName("jingdian_order")
+public class JingdianOrderVO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+    /**
+     * 主键
+     */
+
+    @TableField(value = "id")
+    private Integer id;
+
+
+    /**
+     * 用户
+     */
+
+    @TableField(value = "yonghu_id")
+    private Integer yonghuId;
+
+
+    /**
+     * 旅游景点
+     */
+
+    @TableField(value = "jingdian_id")
+    private Integer jingdianId;
+
+
+    /**
+     * 预定时间
+     */
+    @JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat
+
+    @TableField(value = "jingdian_order_time")
+    private Date jingdianOrderTime;
+
+
+    /**
+     * 预定人数
+     */
+
+    @TableField(value = "jingdian_order_number")
+    private Integer jingdianOrderNumber;
+
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat
+
+    @TableField(value = "create_time")
+    private Date createTime;
+
+
+    /**
+	 * 设置：主键
+	 */
+    public Integer getId() {
+        return id;
+    }
+
+
+    /**
+	 * 获取：主键
+	 */
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    /**
+	 * 设置：用户
+	 */
+    public Integer getYonghuId() {
+        return yonghuId;
+    }
+
+
+    /**
+	 * 获取：用户
+	 */
+
+    public void setYonghuId(Integer yonghuId) {
+        this.yonghuId = yonghuId;
+    }
+    /**
+	 * 设置：旅游景点
+	 */
+    public Integer getJingdianId() {
+        return jingdianId;
+    }
+
+
+    /**
+	 * 获取：旅游景点
+	 */
+
+    public void setJingdianId(Integer jingdianId) {
+        this.jingdianId = jingdianId;
+    }
+    /**
+	 * 设置：预定时间
+	 */
+    public Date getJingdianOrderTime() {
+        return jingdianOrderTime;
+    }
+
+
+    /**
+	 * 获取：预定时间
+	 */
+
+    public void setJingdianOrderTime(Date jingdianOrderTime) {
+        this.jingdianOrderTime = jingdianOrderTime;
+    }
+    /**
+	 * 设置：预定人数
+	 */
+    public Integer getJingdianOrderNumber() {
+        return jingdianOrderNumber;
+    }
+
+
+    /**
+	 * 获取：预定人数
+	 */
+
+    public void setJingdianOrderNumber(Integer jingdianOrderNumber) {
+        this.jingdianOrderNumber = jingdianOrderNumber;
+    }
+    /**
+	 * 设置：创建时间
+	 */
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+
+    /**
+	 * 获取：创建时间
+	 */
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+}
